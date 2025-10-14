af7aefb08ae0a3744cab5c5501292fce135d2455
Sanika Dange <sgdange2005@gmail.com>
2025-10-14 19:39:54 +0530

    little change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
 
+// Configure Amplify
+Amplify.configure(awsExports);
+
+// Create root and render app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,15 +17,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional performance logging
 reportWebVitals();
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
-ReactDOM.render(<App />, document.getElementById('root'));
